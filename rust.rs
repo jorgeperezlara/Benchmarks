2e4d1c16eb9a1631d9348eaf7ea164bf901f8ef6
Dylan Jurski <jurskidylan@yahoo.com>
2024-06-28 21:37:47 -0400

    One second improvement on my system
    Went from 1m53,609s to 1m52.773s. Will continue to tinker but two seconds allowed me to see it saved to asm instructions

--- rust.rs
+++ rust.rs
@@ -8,7 +8,7 @@ pub fn half(mut num: i64) -> i64 {
 pub fn main() {
   let mut j: i64 = 0;
   for i in 0..10000000000 {
-      j = j + half(i);
+      j += half(i);
   }
-  print!("The result is {}", j);
+  print!("The result is {}\n", j);
 }
