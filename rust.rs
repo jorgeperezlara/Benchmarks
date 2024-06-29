89abb1a14e2e68059e11f1ef939a0a60814bd854
Dylan Jurski <jurskidylan@yahoo.com>
2024-06-28 22:17:00 -0400

    Significant time improvement now
    Now runs in ~5 seconds on my system

--- rust.rs
+++ rust.rs
@@ -1,14 +1,12 @@
+#[inline]
 pub fn half(mut num: i64) -> i64 {
-  if num > 1 {
-      num = half(num / 2);
-  }
-  num
+    if num > 1 {
+        num = half(num / 2);
+    }
+    num
 }
 
 pub fn main() {
-  let mut j: i64 = 0;
-  for i in 0..10000000000 {
-      j += half(i);
-  }
-  print!("The result is {}\n", j);
+    let j: i64 = (0..10000000000).into_iter().map(|x| half(x)).sum();
+    print!("The result is {}\n", j);
 }
